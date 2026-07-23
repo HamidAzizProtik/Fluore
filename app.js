97ab970fa76fa0c55b4470b5c278d121fb2106c3
Hamid Aziz Protik <protikhamidaziz@gmail.com>
2026-07-23 20:45:57 +0600

    added new theme change icon

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let notes = [];
 let editingNoteId = null
 
+const sunIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun-icon lucide-sun"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>`;
+const moonIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon-icon lucide-moon"><path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"/></svg>`;
+
 function loadNotes() {
     const savedNotes = localStorage.getItem("fluoreNotes");
     return savedNotes ? JSON.parse(savedNotes) : []
@@ -149,14 +152,18 @@ function closeNoteDialog() {
 
 function ToggleTheme() {
     const isDark = document.body.classList.toggle('dark-theme');
-    localStorage.setItem('theme',isDark ? 'dark' : 'light');
-    document.getElementById('themeToggleBtn').textContent = isDark ? '🏙️' : '🌆'
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+    document.getElementById('themeToggleBtn').innerHTML = isDark ? sunIcon : moonIcon;
 }
 
 function applyStoredTheme() {
-    if(localStorage.getItem('theme') === 'dark') {
-        document.body.classList.add('dark-theme')
-        document.getElementById('themeToggleBtn').textContent = '🏙️'
+    const themeToggleBtn = document.getElementById('themeToggleBtn');
+    
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-theme');
+        themeToggleBtn.innerHTML = sunIcon;
+    } else {
+        themeToggleBtn.innerHTML = moonIcon;
     }
 }
 
